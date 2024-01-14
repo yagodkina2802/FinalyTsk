dc939907b6e3e5c90d9fa7be27155afc3c20e29f
AnastasiaRekalo <global-rekalonastya@gmail.com>
2024-01-14 21:58:15 +0300

    Создание метода для создания второго массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5] {"Hello", "2", "world", ":-)"};
+string[] array1 = new string[array.Length];
+void GetArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array1[count] = array[i];
+        count++;
+        }
+    }
+}
